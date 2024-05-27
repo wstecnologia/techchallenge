0c902f2da634e695e4fab7cdb1ca1b978f14f50c
Wilson Gonçalves <wilssp@gmail.com>
2024-05-27 15:02:28 -0300

    Revisão da Routes

--- src/app/Routes.ts
+++ src/app/Routes.ts
@@ -1,9 +1,8 @@
-import { Router } from 'express'
-import { AuthenticationController } from '@/adapters/in/external/Authentication/AuthenticationController'
-import { AuthenticateUserUseCase } from '@/core/authentication/usecases/AuthenticateUserUseCase'
-import { RegisterUserUseCase } from '@/core/authentication/usecases/RegisterUserUseCase'
+import { Router, Request, Response } from 'express'
 import { InMemoryUserRepository } from '@/adapters/out/persistence/Authentication/InMemoryUserRepository'
 import { RegisterUserUseCase } from '@/core/authentication/domain/usecases/RegisterUser.usecase'
+import { AuthenticationUseCase } from '@/core/authentication/domain/usecases/Authentication.usecase'
+import { AuthenticationController } from '@/adapters/in/controllers/Authentication/AuthenticationController'
 
 const router = Router()
 
@@ -13,17 +12,21 @@ const registerUserUseCase = new RegisterUserUseCase(authenticateUserUseCase)
 
 const authController = new AuthenticationController(registerUserUseCase, authenticateUserUseCase)
 
-router.post('/register', authController.register.bind(authController))
-router.post('/login', authController.login.bind(authController))
+router.post('/register', (request: Request, response: Response) =>
+  authController.register(request, response),
+)
+router.post('/login', (request: Request, response: Response) =>
+  authController.login(request, response),
+)
 
-//TechChalleger
+// TechChalleger
 router.post('/customers', (request: Request, response: Response) => {
   response.status(200).json({ message: 'Cliente cadastrado com sucesso' })
 })
 router.get('/customers', (request: Request, response: Response) => {
   let clientes: Object[] = []
   clientes.push({ nome: 'Savio', email: 'saviodba@gmail.com', cpf: '12345678909' })
-  clientes.push({ nome: 'wilson', email: 'wilson@gmail.com', cpf: '12345678909' })
+  clientes.push({ nome: 'Wilson', email: 'wilson@gmail.com', cpf: '12345678909' })
 
   response.status(200).json(clientes)
 })
@@ -33,22 +36,26 @@ router.post('/products', (request: Request, response: Response) => {
 })
 
 router.put('/products', (request: Request, response: Response) => {
-  response.status(200).json({ message: 'Produto Alterado com sucesso' })
+  response.status(200).json({ message: 'Produto alterado com sucesso' })
 })
 
 router.get('/products', (request: Request, response: Response) => {
-  let clientes: Object[] = []
-  clientes.push({ nome: 'Savio', email: 'saviodba@gmail.com', cpf: '12345678909' })
-  clientes.push({ nome: 'wilson', email: 'wilson@gmail.com', cpf: '12345678909' })
+  let produtos: Object[] = []
+  produtos.push({ nome: 'Produto A', descricao: 'Descrição A', preco: 100 })
+  produtos.push({ nome: 'Produto B', descricao: 'Descrição B', preco: 200 })
 
-  response.status(200).json(clientes)
+  response.status(200).json(produtos)
 })
 
 router.delete('/products', (request: Request, response: Response) => {
-  response.status(200).json({ message: 'Produto excluido com sucesso' })
+  response.status(200).json({ message: 'Produto excluído com sucesso' })
 })
 
-router.post('/orders', authController.login.bind(authController))
-router.post('/orders/payment', authController.login.bind(authController))
+router.post('/orders', (request: Request, response: Response) =>
+  authController.login(request, response),
+)
+router.post('/orders/payment', (request: Request, response: Response) =>
+  authController.login(request, response),
+)
 
 export { router as routes }
