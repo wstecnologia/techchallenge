b4919c62baebd8503f4c1214a6c734778f287276
Wilson Gonçalves <wilssp@gmail.com>
2024-06-03 18:42:10 -0300

    InsertCategorias.

---
+++ sql/insertCategorias.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.category (id, name, description) VALUES
+(uuid_generate_v4(), 'Lanche', 'Lanches diversos.'),
+(uuid_generate_v4(), 'Acompanhamento', 'Acompanhamentos conforme sua preferência.'),
+(uuid_generate_v4(), 'Bebida', 'Temos bebidas refrigerantes e sucos.'),
+(uuid_generate_v4(), 'Sobremesa', 'Sobremesas deliciosas.')
